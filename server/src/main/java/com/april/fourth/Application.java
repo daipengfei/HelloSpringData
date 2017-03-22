07b234437be5ab9ed58276565bc18f54557f4ec9
daipengfei <daipengfei2013@gmail.com>
2017-03-22 18:16:28 +0800

    first commit

---
+++ server/src/main/java/com/april/fourth/Application.java
@@ -0,0 +1,18 @@
+package com.april.fourth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by daipengfei
+ * on 2017/3/22.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+}
---
+++ server/src/main/java/com/april/fourth/config/MongoConfig.java
@@ -0,0 +1,14 @@
+package com.april.fourth.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by daipengfei
+ * on 2017/3/22.
+ */
+
+@Configuration
+public class MongoConfig {
+
+}
