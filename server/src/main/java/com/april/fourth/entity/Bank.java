7e21f54a64a6b55657525986da94f0e02465ba64
daipengfei <daipengfei2013@gmail.com>
2017-04-12 18:01:24 +0800

    update

---
+++ server/src/main/java/com/april/fourth/entity/Bank.java
@@ -0,0 +1,11 @@
+package com.april.fourth.entity;
+
+/**
+ * Created by daipengfei
+ * on 2017/4/12.
+ */
+public class Bank {
+   private String accountNumber;
+
+
+}
--- server/src/main/java/com/april/fourth/init/InitService.java
+++ server/src/main/java/com/april/fourth/init/InitService.java
@@ -7,9 +7,11 @@ import com.april.fourth.service.HelloPerson;
 import org.easyrules.api.RulesEngine;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
-import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -36,17 +38,25 @@ public class InitService implements ApplicationRunner {
     @Resource
     private HelloPerson helloPerson;
 
+    @Resource
+    private ElasticsearchTemplate elasticsearchTemplate;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        rulesEngine.fireRules();
+        Criteria criteria = new Criteria();
+        CriteriaQuery query = new CriteriaQuery(criteria);
+        elasticsearchTemplate.queryForList(query,Object.class);
+//        rulesEngine.fireRules();
 //        mongoOps();
     }
 
     private void mongoOps() {
+        org.springframework.data.mongodb.core.query.Criteria criteria =
+                org.springframework.data.mongodb.core.query.Criteria.where("_id").is("ORDER10006");
         PersonDTO personDTO = helloPerson.helloPerson();
         System.out.println(personDTO.getName());
         List<Order> orders = mongoTemplate.find(
-                new Query(Criteria.where("_id").is("ORDER10006")), Order.class);
+                new Query(criteria), Order.class);
         System.out.println(orders);
         mongoTemplate.save(new Order("ORDER10007",new Date(),new GeoJsonPoint(0,1)));
     }
